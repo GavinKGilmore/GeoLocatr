287c152747f58334d533b7f52abe2c75b8bd92d9
Gavin Gilmore <gkgilmore@mines.edu>
2025-04-04 12:12:46 -0600

    updates

--- app/src/main/java/com/example/geolocatr/MainActivity.kt
+++ app/src/main/java/com/example/geolocatr/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.geolocatr
 
+import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.geolocatr.ui.LocationScreen
 import com.example.geolocatr.ui.theme.GeoLocatrTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +22,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val locationState = remember { mutableStateOf<Location?>(null) }
+            val addressState = remember { mutableStateOf("") }
             GeoLocatrTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    LocationScreen(modifier = Modifier.padding(innerPadding),
+                        location = locationState.value,
+                        locationAvailable = true,
+                        onGetLocation = {},
+                        address = addressState.value
                     )
                 }
             }
--- app/src/main/java/com/example/geolocatr/ui/LocationScreen.kt
+++ app/src/main/java/com/example/geolocatr/ui/LocationScreen.kt
@@ -2,8 +2,17 @@ package com.example.geolocatr.ui
 
 import android.location.Location
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.geolocatr.R
+
+// TODO: Step 2
 
 @Composable
 fun LocationScreen(modifier: Modifier = Modifier,
@@ -12,6 +21,49 @@ fun LocationScreen(modifier: Modifier = Modifier,
                    onGetLocation: () -> Unit,
                    address: String) {
     Column {
-        // TODO: Part 1.I 
+        // TODO: Part 1.I
+        // TODO: integrate null checks
+        if(location != null) {
+            Text(stringResource(R.string.lat_long))
+            Text(stringResource(R.string.lat_long_display, location.latitude, location.longitude))
+            Text(stringResource(R.string.address))
+            Text(address)
+            Button(enabled = locationAvailable,
+                onClick = onGetLocation,
+                ) {
+                Text(stringResource(R.string.get_current_location))
+            }
+        } else {
+            Text(stringResource(R.string.lat_long))
+            Text(stringResource(R.string.lat_long_display, 0.0, 0.0))
+            Text(stringResource(R.string.address))
+            Text(address)
+            Button(enabled = locationAvailable,
+                onClick = onGetLocation,
+            ) {
+                Text(stringResource(R.string.get_current_location))
+            }
+        }
+
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewLocationScreen() {
+    val locationState = remember { mutableStateOf<Location?>(null) }
+    val addressState = remember { mutableStateOf("") }
+    LocationScreen(
+        location = locationState.value,
+        locationAvailable = true,
+        onGetLocation = {
+            locationState.value = Location("").apply {
+                latitude = 1.35
+                longitude = 103.87
+            }
+            addressState.value = "Singapore"
+        },
+        address = addressState.value
+    )
 }
